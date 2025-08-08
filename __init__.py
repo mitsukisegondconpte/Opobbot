b398dd1e3c32e0542680b3ccd1370be9bd16a5a8
mitsukisegondconpte <robottsuki@gmail.com>
2025-08-08 15:56:34 -0500

    Ajout de nouveaux fichiers via ZIP upload

--- __init__.py
+++ __init__.py
@@ -1,6 +1,14 @@
 """
-Several extensions Python is missing, such as a proper class to handle a TCP
-communication with support for cancelling the operation, and a utility class
-to read arbitrary binary data in a more comfortable way, with int/strings/etc.
+This module contains several classes regarding network, low level connection
+with Telegram's servers and the protocol used (TCP full, abridged, etc.).
 """
-from .binaryreader import BinaryReader
+from .mtprotoplainsender import MTProtoPlainSender
+from .authenticator import do_authentication
+from .mtprotosender import MTProtoSender
+from .connection import (
+    Connection,
+    ConnectionTcpFull, ConnectionTcpIntermediate, ConnectionTcpAbridged,
+    ConnectionTcpObfuscated, ConnectionTcpMTProxyAbridged,
+    ConnectionTcpMTProxyIntermediate,
+    ConnectionTcpMTProxyRandomizedIntermediate, ConnectionHttp, TcpMTProxy
+)
