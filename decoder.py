c02e430721292d763a8a4c964a5f7d10fd67f688
mitsukisegondconpte <robottsuki@gmail.com>
2025-08-08 15:52:47 -0500

    Ajout de nouveaux fichiers via ZIP upload

--- decoder.py
+++ decoder.py
@@ -6,57 +6,26 @@
 #
 import warnings
 
-from pyasn1 import error
-from pyasn1.codec.streaming import readFromStream
-from pyasn1.codec.ber import decoder
+from pyasn1.codec.cer import decoder
 from pyasn1.type import univ
 
 __all__ = ['decode', 'StreamingDecoder']
 
-SubstrateUnderrunError = error.SubstrateUnderrunError
+
+class BitStringPayloadDecoder(decoder.BitStringPayloadDecoder):
+    supportConstructedForm = False
 
 
-class BooleanPayloadDecoder(decoder.AbstractSimplePayloadDecoder):
-    protoComponent = univ.Boolean(0)
-
-    def valueDecoder(self, substrate, asn1Spec,
-                     tagSet=None, length=None, state=None,
-                     decodeFun=None, substrateFun=None,
-                     **options):
-
-        if length != 1:
-            raise error.PyAsn1Error('Not single-octet Boolean payload')
-
-        for chunk in readFromStream(substrate, length, options):
-            if isinstance(chunk, SubstrateUnderrunError):
-                yield chunk
-
-        byte = chunk[0]
-
-        # CER/DER specifies encoding of TRUE as 0xFF and FALSE as 0x0, while
-        # BER allows any non-zero value as TRUE; cf. sections 8.2.2. and 11.1 
-        # in https://www.itu.int/ITU-T/studygroups/com17/languages/X.690-0207.pdf
-        if byte == 0xff:
-            value = 1
-
-        elif byte == 0x00:
-            value = 0
-
-        else:
-            raise error.PyAsn1Error('Unexpected Boolean payload: %s' % byte)
-
-        yield self._createComponent(asn1Spec, tagSet, value, **options)
+class OctetStringPayloadDecoder(decoder.OctetStringPayloadDecoder):
+    supportConstructedForm = False
 
 
 # TODO: prohibit non-canonical encoding
-BitStringPayloadDecoder = decoder.BitStringPayloadDecoder
-OctetStringPayloadDecoder = decoder.OctetStringPayloadDecoder
 RealPayloadDecoder = decoder.RealPayloadDecoder
 
 TAG_MAP = decoder.TAG_MAP.copy()
 TAG_MAP.update(
-    {univ.Boolean.tagSet: BooleanPayloadDecoder(),
-     univ.BitString.tagSet: BitStringPayloadDecoder(),
+    {univ.BitString.tagSet: BitStringPayloadDecoder(),
      univ.OctetString.tagSet: OctetStringPayloadDecoder(),
      univ.Real.tagSet: RealPayloadDecoder()}
 )
@@ -77,6 +46,8 @@ class SingleItemDecoder(decoder.SingleItemDecoder):
     TAG_MAP = TAG_MAP
     TYPE_MAP = TYPE_MAP
 
+    supportIndefLength = False
+
 
 class StreamingDecoder(decoder.StreamingDecoder):
     __doc__ = decoder.StreamingDecoder.__doc__
@@ -90,16 +61,16 @@ class Decoder(decoder.Decoder):
     STREAMING_DECODER = StreamingDecoder
 
 
-#: Turns CER octet stream into an ASN.1 object.
+#: Turns DER octet stream into an ASN.1 object.
 #:
-#: Takes CER octet-stream and decode it into an ASN.1 object
+#: Takes DER octet-stream and decode it into an ASN.1 object
 #: (e.g. :py:class:`~pyasn1.type.base.PyAsn1Item` derivative) which
 #: may be a scalar or an arbitrary nested structure.
 #:
 #: Parameters
 #: ----------
 #: substrate: :py:class:`bytes`
-#:     CER octet-stream
+#:     DER octet-stream
 #:
 #: Keyword Args
 #: ------------
@@ -111,7 +82,7 @@ class Decoder(decoder.Decoder):
 #: Returns
 #: -------
 #: : :py:class:`tuple`
-#:     A tuple of pyasn1 object recovered from CER substrate (:py:class:`~pyasn1.type.base.PyAsn1Item` derivative)
+#:     A tuple of pyasn1 object recovered from DER substrate (:py:class:`~pyasn1.type.base.PyAsn1Item` derivative)
 #:     and the unprocessed trailing portion of the *substrate* (may be empty)
 #:
 #: Raises
@@ -121,21 +92,21 @@ class Decoder(decoder.Decoder):
 #:
 #: Examples
 #: --------
-#: Decode CER serialisation without ASN.1 schema
+#: Decode DER serialisation without ASN.1 schema
 #:
 #: .. code-block:: pycon
 #:
-#:    >>> s, _ = decode(b'0\x80\x02\x01\x01\x02\x01\x02\x02\x01\x03\x00\x00')
+#:    >>> s, _ = decode(b'0\t\x02\x01\x01\x02\x01\x02\x02\x01\x03')
 #:    >>> str(s)
 #:    SequenceOf:
 #:     1 2 3
 #:
-#: Decode CER serialisation with ASN.1 schema
+#: Decode DER serialisation with ASN.1 schema
 #:
 #: .. code-block:: pycon
 #:
 #:    >>> seq = SequenceOf(componentType=Integer())
-#:    >>> s, _ = decode(b'0\x80\x02\x01\x01\x02\x01\x02\x02\x01\x03\x00\x00', asn1Spec=seq)
+#:    >>> s, _ = decode(b'0\t\x02\x01\x01\x02\x01\x02\x02\x01\x03', asn1Spec=seq)
 #:    >>> str(s)
 #:    SequenceOf:
 #:     1 2 3
