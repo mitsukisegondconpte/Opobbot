ca20b868e60b26590f242106a1af195d64b6fc77
mitsukisegondconpte <robottsuki@gmail.com>
2025-08-08 15:56:51 -0500

    Ajout de nouveaux fichiers via ZIP upload

--- __init__.py
+++ __init__.py
@@ -1,12 +1,4 @@
-from .connection import Connection
-from .tcpfull import ConnectionTcpFull
-from .tcpintermediate import ConnectionTcpIntermediate
-from .tcpabridged import ConnectionTcpAbridged
-from .tcpobfuscated import ConnectionTcpObfuscated
-from .tcpmtproxy import (
-    TcpMTProxy,
-    ConnectionTcpMTProxyAbridged,
-    ConnectionTcpMTProxyIntermediate,
-    ConnectionTcpMTProxyRandomizedIntermediate
-)
-from .http import ConnectionHttp
+from .abstract import Session
+from .memory import MemorySession
+from .sqlite import SQLiteSession
+from .string import StringSession
