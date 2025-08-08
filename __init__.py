8b4302b7696990fda9bc74f717ff80a93d811003
mitsukisegondconpte <robottsuki@gmail.com>
2025-08-08 15:55:34 -0500

    Ajout de nouveaux fichiers via ZIP upload

--- __init__.py
+++ __init__.py
@@ -1,13 +1,3 @@
-from .client.telegramclient import TelegramClient
-from .network import connection
-from .tl.custom import Button
-from .tl import patched as _  # import for its side-effects
-from . import version, events, utils, errors, types, functions, custom
-
-__version__ = version.__version__
-
-__all__ = [
-    'TelegramClient', 'Button',
-    'types', 'functions', 'custom', 'errors',
-    'events', 'utils', 'connection'
-]
+from .entitycache import EntityCache
+from .messagebox import MessageBox, GapError, PrematureEndReason
+from .session import SessionState, ChannelState, Entity, EntityType
