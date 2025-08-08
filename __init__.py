cc2f55eb12a7f3226eb126e6c0682b6a7fb22259
mitsukisegondconpte <robottsuki@gmail.com>
2025-08-08 15:54:36 -0500

    Ajout de nouveaux fichiers via ZIP upload

--- __init__.py
+++ __init__.py
@@ -1,69 +1,14 @@
-# SPDX-License-Identifier: MIT
+__version__ = "2.6.1"
 
-from attr import (
-    NOTHING,
-    Attribute,
-    AttrsInstance,
-    Converter,
-    Factory,
-    NothingType,
-    _make_getattr,
-    assoc,
-    cmp_using,
-    define,
-    evolve,
-    field,
-    fields,
-    fields_dict,
-    frozen,
-    has,
-    make_class,
-    mutable,
-    resolve_types,
-    validate,
+from .impl import start_connection
+from .types import AddrInfoType, SocketFactoryType
+from .utils import addr_to_addr_infos, pop_addr_infos_interleave, remove_addr_infos
+
+__all__ = (
+    "AddrInfoType",
+    "SocketFactoryType",
+    "addr_to_addr_infos",
+    "pop_addr_infos_interleave",
+    "remove_addr_infos",
+    "start_connection",
 )
-from attr._next_gen import asdict, astuple
-
-from . import converters, exceptions, filters, setters, validators
-
-
-__all__ = [
-    "NOTHING",
-    "Attribute",
-    "AttrsInstance",
-    "Converter",
-    "Factory",
-    "NothingType",
-    "__author__",
-    "__copyright__",
-    "__description__",
-    "__doc__",
-    "__email__",
-    "__license__",
-    "__title__",
-    "__url__",
-    "__version__",
-    "__version_info__",
-    "asdict",
-    "assoc",
-    "astuple",
-    "cmp_using",
-    "converters",
-    "define",
-    "evolve",
-    "exceptions",
-    "field",
-    "fields",
-    "fields_dict",
-    "filters",
-    "frozen",
-    "has",
-    "make_class",
-    "mutable",
-    "resolve_types",
-    "setters",
-    "validate",
-    "validators",
-]
-
-__getattr__ = _make_getattr(__name__)
